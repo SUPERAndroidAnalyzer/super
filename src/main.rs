765999afc8613cd71175bd02e7d886bb56b61ce1
Razican <razican@protonmail.ch>
2016-10-09 17:07:18 +0200

    Added lints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// #![forbid(missing_docs, warnings)]
+#![deny(deprecated, improper_ctypes, non_shorthand_field_patterns, overflowing_literals,
+    plugin_as_library, private_no_mangle_fns, private_no_mangle_statics, stable_features,
+    unconditional_recursion, unknown_lints, unused, unused_allocation, unused_attributes,
+    unused_comparisons, unused_features, unused_parens, while_true)]
+#![warn(missing_docs, trivial_casts, trivial_numeric_casts, unused, unused_extern_crates,
+    unused_import_braces, unused_qualifications, unused_results, variant_size_differences)]
+
 #[macro_use]
 extern crate clap;
 extern crate colored;
