SUPERAndroidAnalyzer/super

commit ef7e6b7b8ab17eddf6940576d3eed0c3287c0599

Use `PathBuf` for paths instead of `String` (#70)
* Use `PathBuf` for paths instead of `String`

Fixes #52

* Use multi-platform paths

* Don't allocate PathBuf it's not necessary

* Use `PathBuf` for paths instead of `String`

Fixes #52

* Use multi-platform paths

* Don't allocate PathBuf it's not necessary

src/results/mod.rs
@@ -36,7 +36,7 @@ pub struct Results {
 
 impl Results {
     pub fn init(config: &Config) -> Option<Results> {
-        let path = format!("{}/{}", config.get_results_folder(), config.get_app_id());
+        let path = config.get_results_folder().join(config.get_app_id());
         if !fs::metadata(&path).is_ok() || config.is_force() {
             if fs::metadata(&path).is_ok() {
                 if let Err(e) = fs::remove_dir_all(&path) {
@@ -151,7 +151,7 @@ impl Results {
     }
 
     pub fn generate_report(&self, config: &Config) -> Result<()> {
-        let path = format!("{}/{}", config.get_results_folder(), config.get_app_id());
+        let path = config.get_results_folder().join(config.get_app_id());
         if !file_exists(&path) || config.is_force() {
             if file_exists(&path) {
                 if let Err(e) = fs::remove_dir_all(&path) {
@@ -191,9 +191,9 @@ impl Results {
         if config.is_verbose() {
             println!("Starting JSON report generation. First we create the file.")
         }
-        let mut f = try!(File::create(format!("{}/{}/results.json",
-                                              config.get_results_folder(),
-                                              config.get_app_id())));
+        let mut f = try!(File::create(config.get_results_folder()
+                                            .join(config.get_app_id())
+                                            .join("results.json")));
         if config.is_verbose() {
             println!("The report file has been created. Now it's time to fill it.")
         }
@@ -250,9 +250,9 @@ impl Results {
         if config.is_verbose() {
             println!("Starting HTML report generation. First we create the file.")
         }
-        let mut f = try!(File::create(format!("{}/{}/index.html",
-                                              config.get_results_folder(),
-                                              config.get_app_id())));
+        let mut f = try!(File::create(config.get_results_folder()
+                                            .join(config.get_app_id())
+                                            .join("index.html")));
         if config.is_verbose() {
             println!("The report file has been created. Now it's time to fill it.")
         }
@@ -437,7 +437,7 @@ impl Results {
 
         // Copying JS and CSS files
         try!(copy_folder(config.get_results_template(),
-                         &format!("{}/{}", config.get_results_folder(), config.get_app_id())));
+                         &config.get_results_folder().join(config.get_app_id())));
 
         try!(self.generate_code_html_files(config));
 
@@ -545,9 +545,10 @@ impl Results {
         try!(self.generate_code_html_folder("", config));
         let menu = try!(self.generate_html_src_menu("", config));
 
-        let mut f = try!(fs::File::create(format!("{}/{}/src/index.html",
-                                                  config.get_results_folder(),
-                                                  config.get_app_id())));
+        let mut f = try!(fs::File::create(config.get_results_folder()
+                                                .join(config.get_app_id())
+                                                .join("src")
+                                                .join("index.html")));
 
         try!(f.write_all(b"<!DOCTYPE html>"));
         try!(f.write_all(b"<html lang=\"en\">"));
@@ -579,25 +580,25 @@ impl Results {
            path.as_ref() == Path::new("smali") {
             return Ok(0);
         }
-        let dir_iter = try!(fs::read_dir(&format!("{}/{}/{}",
-                                                  config.get_dist_folder(),
-                                                  config.get_app_id(),
-                                                  path.as_ref().display())));
+        let dir_iter = try!(fs::read_dir(config.get_dist_folder()
+                                               .join(config.get_app_id())
+                                               .join(path.as_ref())));
 
-        try!(fs::create_dir_all(&format!("{}/{}/src/{}",
-                                         config.get_results_folder(),
-                                         config.get_app_id(),
-                                         path.as_ref().display())));
+        try!(fs::create_dir_all(config.get_results_folder()
+                                      .join(config.get_app_id())
+                                      .join("src")
+                                      .join(path.as_ref())));
         let mut count = 0;
 
         for f in dir_iter {
             let f = match f {
                 Ok(f) => f,
                 Err(e) => {
-                    print_warning(format!("There was an error reading the directory {}/{}/{}: {}",
-                                          config.get_dist_folder(),
-                                          config.get_app_id(),
-                                          path.as_ref().display(),
+                    print_warning(format!("There was an error reading the directory {}: {}",
+                                          config.get_dist_folder()
+                                                .join(config.get_app_id())
+                                                .join(path.as_ref())
+                                                .display(),
                                           e),
                                   config.is_verbose());
                     return Err(Error::from(e));
@@ -607,8 +608,7 @@ impl Results {
             match f.path().extension() {
                 Some(e) => {
                     if e.to_string_lossy() == "xml" || e.to_string_lossy() == "java" {
-                        let prefix =
-                            format!("{}/{}/", config.get_dist_folder(), config.get_app_id());
+                        let prefix = config.get_dist_folder().join(config.get_app_id());
                         try!(self.generate_code_html_for(f.path().strip_prefix(&prefix).unwrap(),
                                                          config));
                         count += 1;
@@ -616,8 +616,7 @@ impl Results {
                 }
                 None => {
                     if f.path().is_dir() {
-                        let prefix =
-                            format!("{}/{}/", config.get_dist_folder(), config.get_app_id());
+                        let prefix = config.get_dist_folder().join(config.get_app_id());
 
                         if f.path().strip_prefix(&prefix).unwrap() != Path::new("original") {
                             let f_count = try!(self.generate_code_html_folder(f.path()
@@ -633,10 +632,10 @@ impl Results {
             }
         }
         if count == 0 {
-            try!(fs::remove_dir(&format!("{}/{}/src/{}",
-                                         config.get_results_folder(),
-                                         config.get_app_id(),
-                                         path.as_ref().display())));
+            try!(fs::remove_dir(config.get_results_folder()
+                                               .join(config.get_app_id())
+                                               .join("src")
+                                               .join(path)));
         }
 
         Ok(count)
@@ -646,10 +645,10 @@ impl Results {
                                               dir_path: P,
                                               config: &Config)
                                               -> Result<String> {
-        let iter = try!(fs::read_dir(&format!("{}/{}/src/{}",
-                                              config.get_results_folder(),
-                                              config.get_app_id(),
-                                              dir_path.as_ref().display())));
+        let iter = try!(fs::read_dir(config.get_results_folder()
+                                           .join(config.get_app_id())
+                                           .join("src")
+                                           .join(dir_path.as_ref())));
         let mut menu = String::new();
         menu.push_str("<ul>");
         for entry in iter {
@@ -685,9 +684,9 @@ impl Results {
                             Some(n) => String::from(n.to_string_lossy().borrow() as &str),
                             None => String::new(),
                         };
-                        let prefix = format!("{}/{}/src/",
-                                             config.get_results_folder(),
-                                             config.get_app_id());
+                        let prefix = config.get_results_folder()
+                                           .join(config.get_app_id())
+                                           .join("src");
                         let submenu =
                             match self.generate_html_src_menu(path.strip_prefix(&prefix).unwrap(),
                                                         config) {
@@ -727,14 +726,13 @@ impl Results {
     }
 
     fn generate_code_html_for<P: AsRef<Path>>(&self, path: P, config: &Config) -> Result<()> {
-        let mut f_in = try!(File::open(format!("{}/{}/{}",
-                                               config.get_dist_folder(),
-                                               config.get_app_id(),
-                                               path.as_ref().display())));
-        let mut f_out = try!(File::create(format!("{}/{}/src/{}.html",
-                                                  config.get_results_folder(),
-                                                  config.get_app_id(),
-                                                  path.as_ref().display())));
+        let mut f_in = try!(File::open(config.get_results_folder()
+                                                .join(config.get_app_id())
+                                                .join(path.as_ref())));
+        let mut f_out = try!(File::create(config.get_results_folder()
+                                                .join(config.get_app_id())
+                                                .join("src")
+                                                .join(path.as_ref())));
 
         let mut code = String::new();
         try!(f_in.read_to_string(&mut code));

src/decompilation.rs
@@ -6,13 +6,13 @@ use std::process::{Command, exit};
 use colored::Colorize;
 use zip::ZipArchive;
 
-use {Error, Config, print_error, print_warning, file_exists};
+use {Error, Config, print_error, print_warning};
 use results::Benchmark;
 
 pub fn decompress(config: &Config) {
-    let path = format!("{}/{}", config.get_dist_folder(), config.get_app_id());
-    if !file_exists(&path) || config.is_force() {
-        if file_exists(&path) {
+    let path = config.get_dist_folder().join(config.get_app_id());
+    if !path.exists() || config.is_force() {
+        if path.exists() {
             if config.is_verbose() {
                 println!("The application decompression folder exists. But no more…");
             }
@@ -36,11 +36,9 @@ pub fn decompress(config: &Config) {
             .arg("d")
             .arg("-s")
             .arg("-o")
-            .arg(format!("{}/{}", config.get_dist_folder(), config.get_app_id()))
+            .arg(&path)
             .arg("-f")
-            .arg(format!("{}/{}.apk",
-                         config.get_downloads_folder(),
-                         config.get_app_id()))
+            .arg(config.get_apk_file())
             .output();
 
         let output = match output {
@@ -63,9 +61,7 @@ pub fn decompress(config: &Config) {
 
         if config.is_verbose() {
             println!("{}",
-                     format!("The application has been decompressed in {}/{}.",
-                             config.get_dist_folder(),
-                             config.get_app_id())
+                     format!("The application has been decompressed in {}.", path.display())
                          .green());
         } else if !config.is_quiet() {
             println!("Application decompressed.");
@@ -77,10 +73,7 @@ pub fn decompress(config: &Config) {
 }
 
 pub fn extract_dex(config: &Config, benchmarks: &mut Vec<Benchmark>) {
-    if config.is_force() ||
-       !file_exists(format!("{}/{}/classes.jar",
-                            config.get_dist_folder(),
-                            config.get_app_id())) {
+    if config.is_force() || !config.get_dist_folder().join(config.get_app_id()).exists() {
         if config.is_verbose() {
             println!("");
             println!("To decompile the app, first we need to extract the {} file.",
@@ -89,9 +82,7 @@ pub fn extract_dex(config: &Config, benchmarks: &mut Vec<Benchmark>) {
 
         let start_time = Instant::now();
 
-        let zip = ZipArchive::new(match File::open(format!("{}/{}.apk",
-                                                           config.get_downloads_folder(),
-                                                           config.get_app_id())) {
+        let zip = ZipArchive::new(match File::open(config.get_apk_file()) {
             Ok(f) => f,
             Err(e) => {
                 print_error(format!("There was an error when decompressing the {} file. More \
@@ -124,9 +115,9 @@ pub fn extract_dex(config: &Config, benchmarks: &mut Vec<Benchmark>) {
             }
         };
 
-        let mut out_file = match File::create(format!("{}/{}/classes.dex",
-                                                      config.get_dist_folder(),
-                                                      config.get_app_id())) {
+        let mut out_file = match File::create(config.get_dist_folder()
+                                                    .join(config.get_app_id())
+                                                    .join("classes.dex")) {
             Ok(f) => f,
             Err(e) => {
                 print_error(format!("There was an error while creating classes.dex file. More \
@@ -183,28 +174,21 @@ pub fn extract_dex(config: &Config, benchmarks: &mut Vec<Benchmark>) {
 }
 
 fn dex_to_jar(config: &Config) {
-    let output;
-    if cfg!(target_family = "windows") {
-        output = Command::new(format!("{}\\d2j-dex2jar.bat", config.get_dex2jar_folder()))
-            .arg(format!("{}\\{}\\classes.dex",
-                         config.get_dist_folder(),
-                         config.get_app_id()))
-            .arg("-o")
-            .arg(format!("{}\\{}\\classes.jar",
-                         config.get_dist_folder(),
-                         config.get_app_id()))
-            .output();
-    } else {
-        output = Command::new(format!("{}/d2j-dex2jar.sh", config.get_dex2jar_folder()))
-            .arg(format!("{}/{}/classes.dex",
-                         config.get_dist_folder(),
-                         config.get_app_id()))
-            .arg("-o")
-            .arg(format!("{}/{}/classes.jar",
-                         config.get_dist_folder(),
-                         config.get_app_id()))
-            .output();
-    }
+    let classes = config.get_dist_folder()
+                        .join(config.get_app_id())
+                        .join("classes.jar");
+    let output = Command::new(config.get_dex2jar_folder()
+                                    .join(if cfg!(target_family="windows") {
+                                         "d2j-dex2jar.bat"
+                                    } else {
+                                        "d2j-dex2jar.sh"
+                                    }))
+        .arg(config.get_dist_folder()
+                   .join(config.get_app_id())
+                   .join("classes.dex"))
+        .arg("-o")
+        .arg(&classes)
+        .output();
 
     if output.is_err() {
         print_error(format!("There was an error when executing the {} to {} conversion \
@@ -232,10 +216,7 @@ fn dex_to_jar(config: &Config) {
                  format!("The application {} {} {}",
                          ".jar".italic(),
                          "file has been generated in".green(),
-                         format!("{}/{}/classes.jar.",
-                                 config.get_dist_folder(),
-                                 config.get_app_id())
-                             .green())
+                         format!("{}", classes.display()).green())
                      .green());
     } else if !config.is_quiet() {
         println!("Jar file generated.");
@@ -243,16 +224,16 @@ fn dex_to_jar(config: &Config) {
 }
 
 pub fn decompile(config: &Config) {
-    let out_path = format!("{}/{}/classes",
-                           config.get_dist_folder(),
-                           config.get_app_id());
-    if config.is_force() || !file_exists(&out_path) {
+    let out_path = config.get_dist_folder()
+                         .join(config.get_app_id())
+                         .join("classes");
+    if config.is_force() || !out_path.exists() {
         let output = Command::new("java")
             .arg("-jar")
             .arg(config.get_jd_cmd_file())
-            .arg(format!("{}/{}/classes.jar",
-                         config.get_dist_folder(),
-                         config.get_app_id()))
+            .arg(config.get_dist_folder()
+                       .join(config.get_app_id())
+                       .join("classes.jar"))
             .arg("-od")
             .arg(&out_path)
             .output();

src/main.rs
@@ -85,7 +85,7 @@ fn main() {
         error_string.push_str("The configuration was loaded, in order, from the following \
                                files:\n\t- Default built-in configuration\n");
         for file in config.get_loaded_config_files() {
-            error_string.push_str(&format!("\t- {}\n", file));
+            error_string.push_str(&format!("\t- {}\n", file.display()));
         }
         print_error(error_string, verbose);
         exit(Error::Config.into());
@@ -196,9 +196,9 @@ fn main() {
         }
 
         if config.is_open() {
-            let report_path = format!("{}/{}/index.html",
-                                      config.get_results_folder(),
-                                      config.get_app_id());
+            let report_path = config.get_results_folder()
+			            .join(config.get_app_id())
+				    .join("index.html");
             if let Err(e) = open::that(report_path) {
                 print_error(format!("Report could not be opened automatically: {}", e),
                             config.is_verbose());

src/results/utils.rs
@@ -150,11 +150,7 @@ pub struct FingerPrint {
 
 impl FingerPrint {
     pub fn new(config: &Config) -> Result<FingerPrint> {
-        let path = format!("{}/{}.apk",
-                           config.get_downloads_folder(),
-                           config.get_app_id());
-
-        let mut f = try!(File::open(path));
+        let mut f = try!(File::open(config.get_apk_file()));
         let mut buffer = Vec::with_capacity(f.metadata().unwrap().len() as usize);
         try!(f.read_to_end(&mut buffer));
 

src/static_analysis/certificate.rs
@@ -34,9 +34,10 @@ pub fn certificate_analysis(config: &Config, results: &mut Results) -> Result<()
         println!("Reading and analyzing the certificates...")
     }
 
-    let path = format!("{}/{}/original/META-INF/",
-                       config.get_dist_folder(),
-                       config.get_app_id());
+    let path = config.get_dist_folder()
+                     .join(config.get_app_id())
+                     .join("original")
+                     .join("META-INF");
     let dir_iter = try!(fs::read_dir(&path));
 
     for f in dir_iter {
@@ -44,11 +45,9 @@ pub fn certificate_analysis(config: &Config, results: &mut Results) -> Result<()
             Ok(f) => f,
             Err(e) => {
                 print_warning(format!("An error occurred when reading the \
-                                       {}/{}/original/META-INF/ dir searching certificates. \
+                                       {} dir searching certificates. \
                                        Certificate analysis will be skipped. More info: {}",
-                                      config.get_dist_folder(),
-                                      config.get_app_id(),
-                                      e),
+                                      path.display(), e),
                               config.is_verbose());
                 break;
             }

src/static_analysis/code.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::fs::{File, DirEntry};
 use std::io::Read;
 use std::str::FromStr;
-use std::path::{Path, PathBuf};
+use std::path::{Path};
 use std::borrow::Borrow;
 use std::thread;
 use std::sync::{Arc, Mutex};
@@ -48,7 +48,7 @@ pub fn code_analysis(manifest: Option<Manifest>, config: &Config, results: &mut 
     let found_vulns: Arc<Mutex<Vec<Vulnerability>>> = Arc::new(Mutex::new(Vec::new()));
     let files = Arc::new(Mutex::new(files));
     let verbose = config.is_verbose();
-    let dist_folder = Arc::new(format!("{}/{}", config.get_dist_folder(), config.get_app_id()));
+    let dist_folder = Arc::new(config.get_dist_folder().join(config.get_app_id()));
 
     if config.is_verbose() {
         println!("Starting analysis of the code with {} threads. {} files to go!",
@@ -75,7 +75,7 @@ pub fn code_analysis(manifest: Option<Manifest>, config: &Config, results: &mut 
                         Some(f) => {
                             if let Err(e) =
                                    analyze_file(f.path(),
-                                                PathBuf::from(thread_dist_folder.as_str()),
+                                                &*thread_dist_folder,
                                                 &thread_rules,
                                                 &thread_manifest,
                                                 &thread_vulns,
@@ -142,8 +142,8 @@ pub fn code_analysis(manifest: Option<Manifest>, config: &Config, results: &mut 
     }
 }
 
-fn analyze_file<P: AsRef<Path>>(path: P,
-                                dist_folder: P,
+fn analyze_file<P: AsRef<Path>, T: AsRef<Path>>(path: P,
+                                dist_folder: T,
                                 rules: &Vec<Rule>,
                                 manifest: &Option<Manifest>,
                                 results: &Mutex<Vec<Vulnerability>>,
@@ -276,17 +276,15 @@ fn add_files_to_vec<P: AsRef<Path>>(path: P,
        path.as_ref() == Path::new("smali") {
         return Ok(());
     }
-    let real_path = format!("{}/{}/{}",
-                            config.get_dist_folder(),
-                            config.get_app_id(),
-                            path.as_ref().display());
+    let real_path = config.get_dist_folder()
+                          .join(config.get_app_id())
+                          .join(path);
     for f in try!(fs::read_dir(&real_path)) {
         let f = match f {
             Ok(f) => f,
             Err(e) => {
                 print_warning(format!("There was an error reading the directory {}: {}",
-                                      &real_path,
-                                      e),
+                                      real_path.display(), e),
                               config.is_verbose());
                 return Err(Error::from(e));
             }
@@ -294,11 +292,10 @@ fn add_files_to_vec<P: AsRef<Path>>(path: P,
         let f_type = try!(f.file_type());
         let f_path = f.path();
         let f_ext = f_path.extension();
-        if f_type.is_dir() && f_path != Path::new(&format!("{}/original", real_path)) {
+        if f_type.is_dir() && f_path != real_path.join("original") {
             try!(add_files_to_vec(f.path()
-                                      .strip_prefix(&format!("{}/{}",
-                                                             config.get_dist_folder(),
-                                                             config.get_app_id()))
+                                      .strip_prefix(&config.get_dist_folder()
+                                                           .join(config.get_app_id()))
                                       .unwrap(),
                                   vec,
                                   config));

src/static_analysis/manifest.rs
@@ -18,7 +18,7 @@ pub fn manifest_analysis(config: &Config, results: &mut Results) -> Option<Manif
     }
 
     let manifest =
-        match Manifest::load(format!("{}/{}/", config.get_dist_folder(), config.get_app_id()),
+        match Manifest::load(config.get_dist_folder().join(config.get_app_id()),
                              config,
                              results) {
             Ok(m) => {
@@ -52,7 +52,7 @@ pub fn manifest_analysis(config: &Config, results: &mut Results) -> Option<Manif
             println!("This does not mean that something went wrong, but it's supposed to have \
                       the application in the format {{package}}.apk in the {} folder and use the \
                       package as the application ID for this auditor.",
-                     config.get_downloads_folder());
+                     config.get_downloads_folder().display());
         }
     }
 

commit 708c80c9c1b8310a61a0c2357470b7cd0bcf295b

Fixed bug introduced in ef7e6b7b8ab17eddf6940576d3eed0c3287c0599

src/results/mod.rs
@@ -727,13 +727,15 @@ impl Results {
     }
 
     fn generate_code_html_for<P: AsRef<Path>>(&self, path: P, config: &Config) -> Result<()> {
-        let mut f_in = try!(File::open(config.get_results_folder()
+        let mut f_in = try!(File::open(config.get_dist_folder()
             .join(config.get_app_package())
             .join(path.as_ref())));
-        let mut f_out = try!(File::create(config.get_results_folder()
-            .join(config.get_app_package())
-            .join("src")
-            .join(path.as_ref())));
+        let mut f_out = try!(File::create(format!("{}.html",
+                                                  config.get_results_folder()
+                                                      .join(config.get_app_package())
+                                                      .join("src")
+                                                      .join(path.as_ref())
+                                                      .display())));
 
         let mut code = String::new();
         let _ = try!(f_in.read_to_string(&mut code));