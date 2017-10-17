1c085fd70085689d0ab6aa76df7bccfbe298ccde
C-Bouthoorn <cbouthoorn@gmail.com>
2017-10-17 17:37:49 +0200

    Make clippy happier

--- src/config.rs
+++ src/config.rs
@@ -124,6 +124,7 @@ impl ConfigDeserializer {
 
         match deser_result {
             toml::value::Value::Table(ref table) => {
+<<<<<<< HEAD
                 let criticality_str = table
                     .get("criticality")
                     .and_then(|v| v.as_str())
@@ -140,6 +141,14 @@ impl ConfigDeserializer {
                             "Description field not found for unknown permission",
                         )
                     })?;
+=======
+                let criticality_str = table.get("criticality").and_then(|v| v.as_str()).ok_or_else(
+                    || serde::de::Error::custom("Criticality field not found for unknown permission"),
+                )?;
+                let string = table.get("description").and_then(|v| v.as_str()).ok_or_else(
+                    || serde::de::Error::custom("Description field not found for unknown permission"),
+                )?;
+>>>>>>> Make clippy happier
 
                 let criticality = Criticality::from_str(criticality_str).map_err(|_| {
                     serde::de::Error::custom(format!(
