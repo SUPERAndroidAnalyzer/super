33a406740e4637ad565ed7247dd1d1c3c72efd6f
Razican <razican@protonmail.ch>
2017-10-24 22:36:00 +0200

    Trying once again

--- travis-helper.sh
+++ travis-helper.sh
@@ -44,7 +44,7 @@ elif [ "$action" = "upload_code_coverage" ]; then
     sudo make install &&
     cd ../.. &&
     rm -rf kcov-master &&
-    mkdir -p "target/cov/super-analyzer"; kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/super-analyzer" "super-analyzer" &&
+    mkdir -p "target/cov/super-analyzer"; kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/super-analyzer" "target/debug/super-analyzer" &&
     for file in target/debug/super_analyzer*[^\.d]; do mkdir -p "target/cov/$(basename $file)"; kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"; done &&
     bash <(curl -s https://codecov.io/bash) &&
     echo "Uploaded code coverage"
