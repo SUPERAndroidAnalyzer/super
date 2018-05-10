d86464ed401c4f9351d6efedd9e237722df01623
Razican <razican@protonmail.ch>
2018-05-10 19:00:03 +0200

    Documented main.rs, closes #54

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 //! SUPER Android Analyzer launcher.
+//!
+//! This code controls the CLI of the application and launches the analysis of the application
+//! using the core library.
 
 #![cfg_attr(feature = "cargo-clippy", deny(clippy))]
 #![forbid(anonymous_parameters)]
@@ -27,14 +30,21 @@ use log::Level;
 use super_analyzer_core::*;
 
 /// Program entry point.
+///
+/// This function will just call the `run()` function and report any fatal error that comes out
+/// of it. It will also exit with a non-zero exit code if things go wrong.
 fn main() {
+    // Call the `run()` function and check for errors.
     if let Err(e) = run() {
         error!("{}", e);
 
+        // After printing the error, print the causes, in order.
         for e in e.causes().skip(1) {
             println!("\t{}{}", "Caused by: ".bold(), e);
         }
 
+        // If the verbose mode is not enabled, we add a message so that the user knows that can
+        // get further information with the `-v` flag in the CLI.
         if !log_enabled!(Level::Debug) {
             println!(
                 "If you need more information, try to run the program again with the {} flag.",
@@ -42,26 +52,35 @@ fn main() {
             );
         }
 
+        // Exit with a non-zero exit code.
         ::std::process::exit(1);
     }
 }
 
-/// Analyzer executable code.
+/// Execute the analysis.
+///
+/// This runs the actual analysis. It checks the CLI, creates the logger, loads the configuration
+/// and if everything goes well, it starts the analysis. It also runs benchmarks and shows the
+/// results.
 fn run() -> Result<(), Error> {
+    // Check the CLI arguments.
     let cli = cli::generate().get_matches();
     let verbose = cli.is_present("verbose");
+    // Initialize all loggers, specifying if the user wanted verbose mode.
     initialize_logger(verbose);
 
+    // Load the configuration.
     let mut config = initialize_config(&cli)?;
 
+    // Check the configuration and return an error with the loaded files.
     if !config.check() {
-        let mut error_string = String::from("Configuration errors were found:\n");
+        let mut error_string = String::from("configuration errors were found:\n");
         for error in config.errors() {
             error_string.push_str(&error);
             error_string.push('\n');
         }
         error_string.push_str(
-            "The configuration was loaded, in order, from the following files: \
+            "the configuration was loaded, in order, from the following files: \
              \n\t- Default built-in configuration\n",
         );
         for file in config.loaded_config_files() {
@@ -73,6 +92,7 @@ fn run() -> Result<(), Error> {
         }.into());
     }
 
+    // Print the banner if we are in verbose mode.
     if config.is_verbose() {
         for c in BANNER.chars() {
             print!("{}", c);
@@ -90,15 +110,18 @@ fn run() -> Result<(), Error> {
         sleep(Duration::from_millis(1250));
     }
 
+    // Start benchmarks.
     let mut benchmarks = BTreeMap::new();
 
     let total_start = Instant::now();
+    // Analyze each apk one by one.
     for package in config.app_packages() {
         config.reset_force();
         analyze_package(package, &mut config, &mut benchmarks)
             .context("application analysis failed")?;
     }
 
+    // Print benchmarks if in benchmark mode.
     if config.is_bench() {
         let total_time = Benchmark::new("Total time", total_start.elapsed());
         println!();
