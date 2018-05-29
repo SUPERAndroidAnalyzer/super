434f5f25db9642584693b46dd35dbab1295424e0
Razican <razican@protonmail.ch>
2018-05-29 21:29:24 +0200

    Some tests were running multiple times

--- travis-helper.sh
+++ travis-helper.sh
@@ -4,73 +4,73 @@ action="$1"
 
 if [ "$action" = "install_deps" ]; then
   # Install rustfmt.
-  if [[ !$PACKAGE && "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_RUST_VERSION" == "stable" ]]; then
+  if [[ -z $PACKAGE && "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_RUST_VERSION" == "stable" ]]; then
     rustup component add rustfmt-preview
   fi
 
   # Install Clippy.
-  if [[ !$PACKAGE && "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
+  if [[ -z $PACKAGE && "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
     cargo install clippy --force --verbose || true
   fi
 
 # Build the project with default features.
 elif [ "$action" = "build" ]; then
-  if [[ !$PACKAGE ]]; then
+  if [ -z $PACKAGE ]; then
     cargo build --verbose
   fi
 
 # Package the crate for crates.io distribution.
 elif [ "$action" = "package" ]; then
-  if [[ !$PACKAGE ]]; then
+  if [ -z $PACKAGE ]; then
     cargo package --verbose
   fi
 
 # Run unit and integration tests.
 elif [ "$action" = "test" ]; then
-  if [[ !$PACKAGE ]]; then
+  if [ -z $PACKAGE ]; then
     cargo test --verbose
   fi
 
 # Run ignored unit and integration tests.
 elif [ "$action" = "test_ignored" ]; then
-  if [[ !$PACKAGE && "$TRAVIS_RUST_VERSION" == "stable" ]]; then
+  if [[ -z $PACKAGE && "$TRAVIS_RUST_VERSION" == "stable" ]]; then
     cargo test --verbose -- --ignored
   fi
 
 # Build the project with unstable features.
 elif [ "$action" = "build_unstable" ]; then
-  if [[ !$PACKAGE ]]; then
+  if [[ -z $PACKAGE ]]; then
     cargo build --verbose --features unstable
   fi
 
 # Run unit and integration tests with unstable features.
 elif [ "$action" = "test_unstable" ]; then
-  if [[ !$PACKAGE && "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
+  if [[ -z $PACKAGE && "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
     cargo test --verbose --features unstable
   fi
 
 # Run ignored unit and integration tests with unstable features.
 elif [ "$action" = "test_unstable_ignored" ]; then
-  if [[ !$PACKAGE && "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
+  if [[ -z $PACKAGE && "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
     cargo test --verbose --features unstable -- --ignored
   fi
 
 
 # Run Clippy.
 elif [ "$action" = "clippy_run" ]; then
-  if [[ !$PACKAGE && "$TRAVIS_RUST_VERSION" == "nightly" && "$TRAVIS_OS_NAME" == "linux" ]] && cargo clippy --version; then
+  if [[ -z $PACKAGE && "$TRAVIS_RUST_VERSION" == "nightly" && "$TRAVIS_OS_NAME" == "linux" ]] && cargo clippy --version; then
     cargo clippy --verbose
   fi
 
 # Check formatting.
 elif [ "$action" = "fmt_run" ]; then
-  if [[ !$PACKAGE && "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_RUST_VERSION" == "stable" ]]; then
+  if [[ -z $PACKAGE && "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_RUST_VERSION" == "stable" ]]; then
       cargo fmt --verbose -- --write-mode=diff
   fi
 
 # Upload code coverage report for stable builds in Linux.
 elif [ "$action" = "upload_code_coverage" ]; then
-  if [[ !$PACKAGE && "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_RUST_VERSION" == "stable" ]]; then
+  if [[ -z $PACKAGE && "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_RUST_VERSION" == "stable" ]]; then
     wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz &&
     tar xzf master.tar.gz &&
     cd kcov-master &&
@@ -88,7 +88,7 @@ elif [ "$action" = "upload_code_coverage" ]; then
 
 # Upload development documentation for the develop branch.
 elif [ "$action" = "upload_documentation" ]; then
-  if [[ !$PACKAGE && "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_RUST_VERSION" == "stable" && "$TRAVIS_PULL_REQUEST" = "false" && "$TRAVIS_BRANCH" == "develop" ]]; then
+  if [[ -z $PACKAGE && "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_RUST_VERSION" == "stable" && "$TRAVIS_PULL_REQUEST" = "false" && "$TRAVIS_BRANCH" == "develop" ]]; then
     cargo rustdoc -- --document-private-items &&
     echo "<meta http-equiv=refresh content=0;url=super/index.html>" > target/doc/index.html &&
     git clone https://github.com/davisp/ghp-import.git &&
@@ -98,7 +98,7 @@ elif [ "$action" = "upload_documentation" ]; then
 
 # Runs packaging tests for pull requests, new releases or release preparations in Ubuntu and Fedora.
 elif [ "$action" = "dist_test" ]; then
-  if [[ $PACKAGE ]]; then
+  if [ -n $PACKAGE ]; then
     mkdir releases &&
     docker pull "$PACKAGE:latest" &&
     docker run -d -t -e TAG=$TAG -v $TRAVIS_BUILD_DIR:/root/super --name "$PACKAGE" --privileged "$PACKAGE:latest" "/bin/bash" &&
