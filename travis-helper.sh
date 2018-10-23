d62f5a3101ccad0cb2449f3c77ca3a6829f34b27
Razican <razican@protonmail.ch>
2018-10-23 21:40:07 +0200

    Install clippy from rustup components, and run in stable Rust

--- travis-helper.sh
+++ travis-helper.sh
@@ -3,14 +3,9 @@
 action="$1"
 
 if [ "$action" = "install_deps" ]; then
-  # Install rustfmt.
+  # Install rustfmt and clippy.
   if [[ -z $PACKAGE && "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_RUST_VERSION" == "stable" ]]; then
-    rustup component add rustfmt-preview
-  fi
-
-  # Install Clippy.
-  if [[ -z $PACKAGE && "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
-    cargo install clippy --force --verbose && export CLIPPY=true || export CLIPPY=false
+    rustup component add rustfmt-preview clippy
   fi
 
 # Build the project with default features.
@@ -58,19 +53,19 @@ elif [ "$action" = "test_unstable_ignored" ]; then
 
 # Run Clippy.
 elif [ "$action" = "clippy_run" ]; then
-  if [[ -z $PACKAGE && "$TRAVIS_RUST_VERSION" == "nightly" && "$TRAVIS_OS_NAME" == "linux" && $CLIPPY ]]; then
+  if [[ -z $PACKAGE && "$TRAVIS_RUST_VERSION" == "stable" && "$TRAVIS_OS_NAME" == "linux" ]]; then
     cargo clippy --verbose
   fi
 
 # Check formatting.
 elif [ "$action" = "fmt_run" ]; then
-  if [[ -z $PACKAGE && "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_RUST_VERSION" == "stable" ]]; then
+  if [[ -z $PACKAGE && "$TRAVIS_RUST_VERSION" == "stable" && "$TRAVIS_OS_NAME" == "linux" ]]; then
       cargo fmt --verbose -- --check
   fi
 
 # Upload code coverage report for stable builds in Linux.
 elif [ "$action" = "upload_code_coverage" ]; then
-  if [[ -z $PACKAGE && "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_RUST_VERSION" == "stable" ]]; then
+  if [[ -z $PACKAGE && "$TRAVIS_RUST_VERSION" == "stable" && "$TRAVIS_OS_NAME" == "linux" ]]; then
     wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz &&
     tar xzf master.tar.gz &&
     cd kcov-master &&
@@ -88,7 +83,7 @@ elif [ "$action" = "upload_code_coverage" ]; then
 
 # Upload development documentation for the develop branch.
 elif [ "$action" = "upload_documentation" ]; then
-  if [[ -z $PACKAGE && "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_RUST_VERSION" == "stable" && "$TRAVIS_PULL_REQUEST" = "false" && "$TRAVIS_BRANCH" == "develop" ]]; then
+  if [[ -z $PACKAGE && "$TRAVIS_RUST_VERSION" == "stable" && "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_PULL_REQUEST" = "false" && "$TRAVIS_BRANCH" == "develop" ]]; then
     cargo rustdoc -- --document-private-items &&
     echo "<meta http-equiv=refresh content=0;url=super/index.html>" > target/doc/index.html &&
     git clone https://github.com/davisp/ghp-import.git &&
