f9ea2dad188276c4a5b02874f1d8a841327a2812
Razican <razican@protonmail.ch>
2018-11-03 11:54:27 +0100

    Fixing Windows builds. Not doing the full test, since we don't have Java installed

--- travis-helper.sh
+++ travis-helper.sh
@@ -28,7 +28,7 @@ elif [ "$action" = "test" ]; then
 
 # Run ignored unit and integration tests.
 elif [ "$action" = "test_ignored" ]; then
-  if [[ -z $PACKAGE && "$TRAVIS_RUST_VERSION" == "stable" ]]; then
+  if [[ -z $PACKAGE && "$TRAVIS_RUST_VERSION" == "stable" && "$TRAVIS_OS_NAME" != "windows" ]]; then
     cargo test --verbose -- --ignored
   fi
 
