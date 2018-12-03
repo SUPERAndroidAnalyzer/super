d940607105d36d567b5970439b50f9d68632b346
Razican <razican@protonmail.ch>
2018-12-03 20:32:44 +0100

    Trying Travis-CI stages

--- travis-helper.sh
+++ travis-helper.sh
@@ -1,70 +1,51 @@
 #!/bin/bash
 
 action="$1"
-
-if [ "$action" = "install_deps" ]; then
-  # Install rustfmt and clippy.
-  if [[ -z $PACKAGE && -z $DEPLOY && "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_RUST_VERSION" == "stable" ]]; then
-    rustup component add rustfmt-preview clippy-preview
-  fi
+package="$2"
 
 # Build the project with default features.
 elif [ "$action" = "build" ]; then
-  if [ -z $PACKAGE ] && [ -z $DEPLOY ]; then
-    cargo build --verbose
-  fi
-
-# Package the crate for crates.io distribution.
-elif [ "$action" = "package" ]; then
-  if [ -z $PACKAGE ] && [ -z $DEPLOY ]; then
-    cargo package --verbose
-  fi
+  cargo build --verbose
 
 # Run unit and integration tests.
 elif [ "$action" = "test" ]; then
-  if [ -z $PACKAGE ] && ([ -z $DEPLOY ] || ([ ! -z $DEPLOY ] && [ ! -z $TRAVIS_TAG ])); then
-    cargo test --verbose
-  fi
+  cargo test --verbose
 
 # Run ignored unit and integration tests.
 elif [ "$action" = "test_ignored" ]; then
-  if [[ -z $PACKAGE && "$TRAVIS_RUST_VERSION" == "stable" && "$TRAVIS_OS_NAME" != "windows" && ( -z $DEPLOY || (! -z $DEPLOY  && ! -z $TRAVIS_TAG))]]; then
+  if [[ "$TRAVIS_RUST_VERSION" == "stable" && "$TRAVIS_OS_NAME" != "windows" && (! -z $TRAVIS_TAG) ]]; then
     cargo test --verbose -- --ignored
   fi
 
 # Build the project with unstable features.
 elif [ "$action" = "build_unstable" ]; then
-  if [[ -z $PACKAGE && -z $DEPLOY && "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
-    cargo build --verbose --features unstable
-  fi
+  cargo build --verbose --features unstable
 
 # Run unit and integration tests with unstable features.
 elif [ "$action" = "test_unstable" ]; then
-  if [[ -z $PACKAGE && "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
+  if [[ "$TRAVIS_RUST_VERSION" == "nightly" ]]; then
     cargo test --verbose --features unstable
   fi
 
 # Run ignored unit and integration tests with unstable features.
 elif [ "$action" = "test_unstable_ignored" ]; then
-  if [[ -z $PACKAGE && -z $DEPLOY && "$TRAVIS_RUST_VERSION" == "nightly" && "$TRAVIS_OS_NAME" != "windows" ]]; then
+  if [[ "$TRAVIS_RUST_VERSION" == "nightly" && "$TRAVIS_OS_NAME" != "windows" ]]; then
     cargo test --verbose --features unstable -- --ignored
   fi
 
 # Run Clippy.
 elif [ "$action" = "clippy_run" ]; then
-  if [[ -z $PACKAGE && -z $DEPLOY && "$TRAVIS_RUST_VERSION" == "stable" && "$TRAVIS_OS_NAME" == "linux" ]]; then
-    cargo clippy --verbose
-  fi
+  rustup component add clippy-preview &&
+  cargo clippy --verbose
 
 # Check formatting.
 elif [ "$action" = "fmt_run" ]; then
-  if [[ -z $PACKAGE && -z $DEPLOY && "$TRAVIS_RUST_VERSION" == "stable" && "$TRAVIS_OS_NAME" == "linux" ]]; then
-      cargo fmt --verbose -- --check
-  fi
+  rustup component add rustfmt-preview &&
+  cargo fmt --verbose -- --check
 
 # Upload code coverage report for stable builds in Linux.
 elif [ "$action" = "upload_code_coverage" ]; then
-  if [[ -z $PACKAGE && -z $DEPLOY && "$TRAVIS_RUST_VERSION" == "stable" && "$TRAVIS_OS_NAME" == "linux" ]]; then
+  if [[ "$TRAVIS_RUST_VERSION" == "stable" && "$TRAVIS_OS_NAME" == "linux" ]]; then
     wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz &&
     tar xzf master.tar.gz &&
     cd kcov-master &&
@@ -81,23 +62,19 @@ elif [ "$action" = "upload_code_coverage" ]; then
   fi
 
 # Upload development documentation for the develop branch.
-elif [ "$action" = "upload_documentation" ]; then
-  if [[ -z $PACKAGE && -z $DEPLOY && "$TRAVIS_RUST_VERSION" == "stable" && "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_PULL_REQUEST" = "false" && "$TRAVIS_BRANCH" == "develop" ]]; then
-    cargo rustdoc -- --document-private-items &&
-    echo "<meta http-equiv=refresh content=0;url=super/index.html>" > target/doc/index.html &&
-    git clone https://github.com/davisp/ghp-import.git &&
-    ./ghp-import/ghp_import.py -n -p -f -m "Documentation upload" -r https://"$GH_TOKEN"@github.com/"$TRAVIS_REPO_SLUG.git" target/doc &&
-    echo "Uploaded documentation"
-  fi
+elif [ "$action" = "documentation" ]; then
+  cargo rustdoc -- --document-private-items &&
+  echo "<meta http-equiv=refresh content=0;url=super/index.html>" > target/doc/index.html
 
 # Runs packaging tests for pull requests, new releases or release preparations in Ubuntu and Fedora.
 elif [ "$action" = "dist_test" ]; then
-  if ! [ -z $PACKAGE ]; then
     mkdir -pv releases &&
-    docker pull "$PACKAGE:latest" &&
-    docker run -d -t -e TAG=$TAG -v $TRAVIS_BUILD_DIR:/root/super --name "$PACKAGE" --privileged "$PACKAGE:latest" "/bin/bash" &&
-    docker exec "$PACKAGE" "/root/super/`echo $PACKAGE`_build.sh"
-  elif [ ! -z $DEPLOY ] && [ ! -z $TRAVIS_TAG ]; then
+    docker pull "$package:latest" &&
+    docker run -d -t -e TAG=$TAG -v $TRAVIS_BUILD_DIR:/root/super --name "$package" --privileged "$package:latest" "/bin/bash" &&
+    docker exec "$package" "/root/super/`echo $package`_build.sh"
+
+elif [ "action" = "deploy"]; then
+  if ![-z $TRAVIS_TAG]; then
     mkdir -pv releases &&
     for PACKAGE in "debian" "ubuntu" "fedora" "centos"; do
       docker pull "$PACKAGE:latest" &&
@@ -105,7 +82,6 @@ elif [ "$action" = "dist_test" ]; then
       docker exec "$PACKAGE" "/root/super/`echo $PACKAGE`_build.sh"
     done
   fi
-
 fi
 
 exit $?
