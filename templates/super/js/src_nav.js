5bb1757acc70d7d4d3887ea5a03db3c923ef44cf
Razican <iban.eguia@cern.ch>
2019-08-26 17:22:05 +0200

    Upgraded jQuery

--- templates/super/js/src_nav.js
+++ templates/super/js/src_nav.js
@@ -1,5 +1,5 @@
-$(function() {
-    $("li > a").click(function() {
+$(function () {
+    $("li > a").click(function () {
         if ($(this).parent().hasClass("open")) {
             $(this).parent().removeClass("open");
         } else if ($(this).parent().children("ul").length > 0) {
