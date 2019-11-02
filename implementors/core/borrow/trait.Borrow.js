c617b6ba4efb004b88c14739101f6376c9e06c51
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-02 13:09:19 +0000

    Deploy SUPERAndroidAnalyzer/super to github.com/SUPERAndroidAnalyzer/super.git:gh-pages

--- implementors/core/borrow/trait.Borrow.js
+++ implementors/core/borrow/trait.Borrow.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["generic_array"] = [{text:"impl&lt;T, N&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/borrow/trait.Borrow.html\" title=\"trait core::borrow::Borrow\">Borrow</a>&lt;[T]&gt; for <a class=\"struct\" href=\"generic_array/struct.GenericArray.html\" title=\"struct generic_array::GenericArray\">GenericArray</a>&lt;T, N&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;N: <a class=\"trait\" href=\"generic_array/trait.ArrayLength.html\" title=\"trait generic_array::ArrayLength\">ArrayLength</a>&lt;T&gt;,&nbsp;</span>",synthetic:false,types:["generic_array::GenericArray"]},];
+implementors["toml"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/borrow/trait.Borrow.html\" title=\"trait core::borrow::Borrow\">Borrow</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"toml/struct.Spanned.html\" title=\"struct toml::Spanned\">Spanned</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>&gt;",synthetic:false,types:["toml::spanned::Spanned"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ implementors/synstructure/trait.MacroResult.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["synstructure"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
